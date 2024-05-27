d799aa6e2861a8c4f1e1334da548bde5be420ea5
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-05-27 18:06:58 +1200

    add lighting

--- Assets/Scripts/Environment/PlatformScripts/Bouncer.cs
+++ Assets/Scripts/Environment/PlatformScripts/Bouncer.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 namespace Special2dPlayerController {
     public class Bouncer : MonoBehaviour {
         [SerializeField] private float _bounceForce = 70;
-
+        [SerializeField] private AudioClip _bounceSound;
+        
         private void OnCollisionStay2D(Collision2D other) {
-            if (other.collider.TryGetComponent(out IPlayerController controller)) controller.ApplyVelocity(transform.up * _bounceForce, PlayerForce.Burst);
+            if (other.collider.TryGetComponent(out IPlayerController controller))
+            {
+                AudioSystem.Instance.PlaySound(_bounceSound, vol:0.5f);
+                controller.ApplyVelocity(transform.up * _bounceForce, PlayerForce.Burst);
+            }
         }
     }
 }
